92fc1ddc3f80a1b28fbcc40f967975ec7390f681
kmoreti <kleber.camargo@fatec.sp.gov.br>
2018-10-18 21:33:37 -0300

    Added the observer pattern

---
+++ src/main/java/observer/weather/Observer.java
@@ -0,0 +1,5 @@
+package observer.weather;
+
+public interface Observer {
+    void update(float temperature, float humidity, float pressure);
+}
---
+++ src/main/java/observer/weather/WeatherData.java
@@ -0,0 +1,56 @@
+package observer.weather;
+
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+    private ArrayList<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        this.observers = new ArrayList<>();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        int i = observers.indexOf(observer);
+        if (i >= 0) {
+            observers.remove(i);
+        }
+    }
+
+    @Override
+    public void notifyObserver() {
+        observers.forEach(observer ->
+                observer.update(temperature, humidity, pressure));
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    private void measurementsChanged() {
+        notifyObserver();
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+}
